0b0cca9e6da9449308fb89670f593ded1f5aa6c6
Marko Gargenta <marko@marakana.com>
2012-04-09 14:29:08 -0700

    Initial checkin

---
+++ HelloWorldJava/src/com/intel/hello/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.intel.hello;
+
+/**
+ * This is a class that explains how to write a Hello, World! example.
+ * 
+ * @author marko
+ * @see Java documentation
+ *
+ */
+public class HelloWorld {
+	
+	/**
+	 * This is the main entry point into our Hello, World! example
+	 * assuming command-line execution.
+	 * 
+	 * @param args An array of command-line arguemnts that could be passed in.
+	 * @return Zero if everything went well.
+	 */
+	public static int main(String[] args) {
+		System.out.println("Hello, World!");
+		return 0;
+	}
+	
+}
---
+++ SensorsDemo/src/com/marakana/android/sensors/SensorsDemoActivity.java
@@ -0,0 +1,52 @@
+package com.marakana.android.sensors;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SensorsDemoActivity extends Activity implements
+		SensorEventListener {
+	private SensorManager sensorManager;
+	private Sensor sensor;
+	private TextView out;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main);
+		out = (TextView) findViewById(R.id.out);
+
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		sensorManager.registerListener(this, sensor,
+				SensorManager.SENSOR_DELAY_UI);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		sensorManager.unregisterListener(this);
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		out.setText(String.format("%.2f, %.2f, %.2f", event.values[0],
+				event.values[1], event.values[2]));
+	}
+
+}

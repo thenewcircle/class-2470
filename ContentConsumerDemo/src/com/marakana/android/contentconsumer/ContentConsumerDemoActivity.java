39a2fd3787d860981115b66a4180ecd119a05c46
Marko Gargenta <marko@marakana.com>
2012-04-11 13:06:16 -0700

    Added item.xml, changed to ListActivity

--- ContentConsumerDemo/src/com/marakana/android/contentconsumer/ContentConsumerDemoActivity.java
+++ ContentConsumerDemo/src/com/marakana/android/contentconsumer/ContentConsumerDemoActivity.java
@@ -1,16 +1,16 @@
 package com.marakana.android.contentconsumer;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
-public class ContentConsumerDemoActivity extends Activity {
-	private static final String[] FROM = { Settings.NameValueTable.NAME, Settings.NameValueTable.VALUE };
-	private static final int[] TO = { android.R.id.text1, android.R.id.text2 };
-	private ListView list;
+public class ContentConsumerDemoActivity extends ListActivity {
+	private static final String[] FROM = { Settings.NameValueTable.NAME,
+			Settings.NameValueTable.VALUE, Settings.NameValueTable._ID };
+	private static final int[] TO = { R.id.text_name, R.id.text_value,
+			R.id.text_id };
 	private Cursor cursor;
 	private SimpleCursorAdapter adapter;
 
@@ -18,15 +18,12 @@ public class ContentConsumerDemoActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
 
 		cursor = getContentResolver().query(Settings.System.CONTENT_URI, null,
 				null, null, null);
 
-		adapter = new SimpleCursorAdapter(this,
-				android.R.layout.simple_list_item_2, cursor, FROM, TO);
+		adapter = new SimpleCursorAdapter(this, R.layout.item, cursor, FROM, TO);
 
-		list = (ListView) findViewById(R.id.list);
-		list.setAdapter(adapter);
+		setListAdapter(adapter);
 	}
 }
